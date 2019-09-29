7a466df338733d4f1ff9c6363404e53e7bd5efcd
Iiro Moilanen <t9moii00@students.oamk.fi>
2019-09-29 23:04:54 +0300

    muokkausta osa3

--- session.php
+++ session.php
@@ -1,6 +1,22 @@
 <?php include "menu.php"; ?>
 	<h2>Asiakastiedot</h2>
 	<p>
-		Tähän tulee postilla asiakastiedot
+    Omat tietosi.
+		<form class="" action="show_session.php" method="session">
+      <label for="en">Etunimi</label> <br>
+      <input type="text" id="en" name="en" value="" placeholder="Etunimi"> <br>
+      <label for="sn">Sukunimi</label> <br>
+      <input type="text" id="sn" name="sn" value="" placeholder="Sukunimi"> <br>
+      <label for="po">Postiosoite</label> <br>
+      <input type="text" name="po" value="" placeholder="Postiosoite"> <br>
+      <label for="pn">Postinumero</label> <br>
+      <input type="text" name="pn" value="" placeholder="Postinumero"> <br>
+      <label for="pu">Puhelinnumero</label> <br>
+      <input type="text" name="pu" value="" placeholder="Puhelinnumero"> <br>
+
+      <input type="submit" name="" value="Save">
+    </form>
+
+      </form>
 	</p>
 <?php include "footer.php"; ?>
---
+++ show_session.php
@@ -0,0 +1,7 @@
+<?php include "menu.php"; ?>
+<h2> Omat tietosi </h2>
+<p>
+
+</p>
+<?php print_r ($_POST);?>
+<?php include "footer.php"; ?>
